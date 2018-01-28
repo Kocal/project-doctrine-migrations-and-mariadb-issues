788ac8e746fea36025d31db6ad756a85a6f5fde8
Hugo Alliaume <kocal@live.fr>
2018-01-28 14:47:49 +0100

    Thanks https://stackoverflow.com/a/21068085/7291683 !!!

--- bootstrap.php
+++ bootstrap.php
@@ -7,10 +7,20 @@ require_once "vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
+$paths = array(__DIR__."/src");
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 // or if you prefer yaml or XML
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+
+$cache = new \Doctrine\Common\Cache\ArrayCache();
+$reader = new Doctrine\Common\Annotations\AnnotationReader();
+$driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver($reader, $paths);
+
+$config->setMetadataCacheImpl($cache);
+$config->setQueryCacheImpl($cache);
+$config->setMetadataDriverImpl($driver);
 
 // database configuration parameters
 $conn = array(
